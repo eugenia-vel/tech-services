74396a472f78e94627b881ad44dfa4ef895b683c
eugenia-vel <eugenia_vel@bk.ru>
2025-12-15 03:02:26 +0300

    Добавлен ErrorDecoder

--- tech-services/src/main/java/com/cdpo/techservices/config/TechServiceConfiguration.java
+++ tech-services/src/main/java/com/cdpo/techservices/config/TechServiceConfiguration.java
@@ -1,9 +1,12 @@
 package com.cdpo.techservices.config;
 
+import com.cdpo.techservices.exceptions.DwhException;
+import feign.Response;
 import feign.Retryer;
 import feign.codec.ErrorDecoder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.client.RestClient;
 
 @Configuration
@@ -19,5 +22,18 @@ public class TechServiceConfiguration {
     @Bean
     public Retryer retryer() {
         return new Retryer.Default(1000, 3000, 3);
+    }
+
+    @Bean
+    public ErrorDecoder customErrorDecoder() {
+        return new ErrorDecoder() {
+            @Override
+            public Exception decode(String methodKey, Response response) {
+                if (response.status() == 404) {
+                    return new DwhException(HttpStatus.NOT_FOUND, "Ошибка при отправке данных в сервис сбора статистики");
+                }
+                return null;
+            }
+        };
     }
 }
